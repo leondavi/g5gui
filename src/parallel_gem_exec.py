498fc3e65151d97482b0bc0fdfd8dcb3c6594f7d
david <ddleon@outlook.co.il>
2019-04-13 03:13:24 +0300

    processes numbers hash were added

--- src/parallel_gem_exec.py
+++ src/parallel_gem_exec.py
@@ -7,6 +7,7 @@ class parallel_gem_exec():
         self.gem5_dir = gem5_dir
         self.numof_processes_avaialable = numof_processes_avaialable
         self.processes_list = []
+        self.processes_num_list = []
         self.processes_available_list = self.processes_list
         self.jobs_remain = len(parallel_jobs)
         self.job_iterator = 0
@@ -17,21 +18,29 @@ class parallel_gem_exec():
             newProc = Process(target=self.task)
             self.job_iterator += 1
             self.processes_list.append(newProc)
+            self.processes_num_list.append(self.assign_proc_num())
             newProc.start()
 
     def task(self):
         pass
 
+    def assign_proc_num(self):
+        proc_num = 0
+        while(proc_num in self.processes_num_list):
+            proc_num += 1
 
     def clear_finished_processes(self):
         survived_clear = []
+        survived_clear_nums = []
         for idx,proc in enumerate(self.processes_list):
             proc.join(timeout=0)
             if not proc.is_alive():
                 self.jobs_remain -= 1
             else:
                 survived_clear.append(proc)
+                survived_clear_nums.append(self.processes_num_list[idx])
         self.processes_list = survived_clear
+        self.processes_num_list = survived_clear_nums
 
 
     # Getters
