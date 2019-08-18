80267abafc30578390f8516590248da5bdd60899
David <ddleon@outlook.co.il>
2019-08-18 12:01:16 -0700

    FlexCPU debug-flags support was added

--- src/definitions.py
+++ src/definitions.py
@@ -1,7 +1,9 @@
 NEGNULL = -1
 
 
-DebugModes = ["Activity","Branch","MinorCPU","Decode","MinorExecute","Fetch","MinorInterrupt","MinorMem","MinorScoreboard","MinorTrace","MinorTiming","BGUTrace","No_Debug","All_Traces"]
+#DebugModes = ["Activity","Branch","MinorCPU","Decode","MinorExecute","Fetch","MinorInterrupt","MinorMem","MinorScoreboard","MinorTrace","MinorTiming","BGUTrace","No_Debug","All_Traces"]
+
+DebugModes = ['FlexCPUAll','FlexCPUCoreEvent', 'FlexCPUInstEvent','FlexCPUThreadEvent', 'FlexCPUBranchPred','FlexCPUBufferDump', 'FlexCPUDeps']
 
 DEBUG_OPTION = "--debug-flags"
 
